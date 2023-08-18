d7316f4548eb4fc3bea372176c95f1fa8940d6f9
vc-jinal <jinal.mistry@viitor.cloud>
2023-08-18 15:26:04 +0530

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017', {
+      dbName: 'chat-app',
+    }),
+    ChatModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { Chat, chatSchema } from './schemas/chat.schema';
 import { ChatController } from './chat.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Chat.name, schema: chatSchema }]),
+  ],
   controllers: [ChatController],
   providers: [ChatService],
 })
