d2b35e0031021c9c30bc42c023904205b50ea737
vc-jinal <jinal.mistry@viitor.cloud>
2023-08-24 15:27:16 +0530

    created chat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatController } from './chat/chat.controller';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ChatModule],
+  controllers: [AppController, ChatController],
   providers: [AppService],
 })
 export class AppModule {}
